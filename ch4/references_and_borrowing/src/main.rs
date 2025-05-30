450eafe1e947daeb26f04e935c66bd6010a3a47d
Mohammad Nauman <md.nauman@gmail.com>
2025-05-30 11:16:58 -0700

    references and borrowing

--- ch4/references_and_borrowing/src/main.rs
+++ ch4/references_and_borrowing/src/main.rs
@@ -4,10 +4,67 @@ fn main() {
     let s1 = String::from("hello");
     let len = calculate_length(&s1);
 
-
     println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hola");
+    change(&mut s);
+    println!("s: {s}");
+    {
+        let r1 = &mut s;
+        println!("r1: {r1}");
+    }
+    let r2 = &mut s;
+    println!("r2: {r2}");
+
+    mut_immut_fn();
+    ref_scope_fn();
+    value_scope_fn();
+    let dangle_ref = dangle();
+    println!("dangle_ref: {dangle_ref}");
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn mut_immut_fn() {
+    let s = String::from("hellooo");
+    let r1 = &s;
+    let r2 = &s;
+    //let r3 = &mut s;
+    println!("r1: {r1}");
+    println!("r2: {r2}");
+}
+
+fn ref_scope_fn() {
+    let mut s = String::from("hello from ref_scope_fn");
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("r1: {r1}");
+    println!("r2: {r2}");
+    println!("r1: {r1}");
+
+    let r3 = &mut s;
+    println!("r3: {r3}");
+    
+}
+
+fn value_scope_fn() {
+    let s = String::from("hello from value_scope_fn");
+
+    let r1 = s.clone();
+    let r2 = s;
+    println!("r1: {r1}");
+    println!("r2: {r2}");
+    
+}
+
+fn dangle() -> String {
+    let s = String::from("hello from dangle");
+    s
 }
