e50a97d9f1c493d7b63e05e715ec3578deb0f1a0
Mohammad Nauman <md.nauman@gmail.com>
2025-05-31 06:14:32 -0700

    Slice & dice

--- ch4/slices/src/main.rs
+++ ch4/slices/src/main.rs
@@ -1,15 +1,21 @@
 fn main() {
     
-    let mut s = String::from("Hello World!");
-    let word = first_word(&s);
+    let s = String::from("Hello World!How are!");
+    let word = first_word_index(&s);
     println!("The first word is: {word}");
 
-    s.clear();
+    //s.clear();
     //println!("The first word is: {word}");
+
+    let word1 = first_word_slice(&s);
+    println!("The first word is: {word1}");
+
+    let word2 = second_word_slice(&s);
+    println!("The second word is: {word2}");
 
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word_index(s: &String) -> usize {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate(){
@@ -18,4 +24,34 @@ fn first_word(s: &String) -> usize {
         }
     }
     s.len()
+}
+
+fn first_word_slice(s: &String) -> &str {
+
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
+
+fn second_word_slice(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    let mut first_space = false;
+    let mut m = 0;
+    let mut n = 0;
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' ' {
+            if first_space {
+                n = i;  //second space  
+                return &s[m+1..n];
+            } else {
+                first_space = true;
+                m = i;
+            }
+        }
+    }
+    &s[m+1..]
 }
