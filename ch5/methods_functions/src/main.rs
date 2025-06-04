202ddb89842fcae762b7d476aee7545e4c48abea
Mohammad Nauman <md.nauman@gmail.com>
2025-06-04 06:41:29 -0700

    adding more methods to structs
    impl with the same name as fields

--- ch5/methods_functions/src/main.rs
+++ ch5/methods_functions/src/main.rs
@@ -7,6 +7,13 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    fn height(&self) -> bool {
+        self.height > 0
+    }
 }
 
 fn main() {
@@ -15,4 +22,13 @@ fn main() {
         height: 50,
     };
     println!("Area of rectangle is {} square pixels.", rect1.area());
+
+    println!("\n--------------------------------\n");
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+    if rect1.height() {
+        println!("The rectangle has a nonzero height; it is {}", rect1.height);
+    }
 }
