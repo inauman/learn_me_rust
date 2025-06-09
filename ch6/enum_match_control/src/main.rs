0f78cd0a26457af5d9e5587066c657086b256766
Mohammad Nauman <md.nauman@gmail.com>
2025-06-09 09:47:52 -0700

    Enum Match example

---
+++ ch6/enum_match_control/src/main.rs
@@ -0,0 +1,22 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+
+fn main() {
+    
+    let coin = Coin::Quarter;
+    println!("It's worth {} cents!", value_in_cents(coin));
+}
