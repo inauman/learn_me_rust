d1ecf8f285a9208c5d764e8b4f83573546a94fc3
Mohammad Nauman <md.nauman@gmail.com>
2025-06-08 23:29:22 -0700

    enum dt str: Option<T>

--- ch6/enum_dt_str/src/main.rs
+++ ch6/enum_dt_str/src/main.rs
@@ -12,6 +12,22 @@ enum IpAddr3 {
     V4(u8, u8, u8, u8),
     V6(String),
 }
+
+enum Message {
+    Quite,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+
+        println!("Calling");
+    }
+}
+
+
 //struct IpAddr {
 //    kind: IpAddrKind,
 //    address: String,
@@ -46,4 +62,11 @@ fn main() {
     route(six);
 
     println!("Hello, world!");
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+    let absent_number: Option<i32> = None;
 }
