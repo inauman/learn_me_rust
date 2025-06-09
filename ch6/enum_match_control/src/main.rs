246eb23f92a6728facf8463ea2ab63071506edf8
Mohammad Nauman <md.nauman@gmail.com>
2025-06-09 10:07:15 -0700

    Enum Match binding data

--- ch6/enum_match_control/src/main.rs
+++ ch6/enum_match_control/src/main.rs
@@ -1,22 +1,37 @@
+#[derive(Debug)]
+
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
+
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
 
 
 fn main() {
     
-    let coin = Coin::Quarter;
+    let coin = Coin::Quarter(UsState::Alaska);
     println!("It's worth {} cents!", value_in_cents(coin));
 }
