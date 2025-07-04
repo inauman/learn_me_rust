9f9c2e7059a399599d4d4b5b256c3c58e875a097
Mohammad Nauman <md.nauman@gmail.com>
2025-07-04 09:39:08 -0700

    pkk module paths use -> vector

--- ch7/restaurant/src/lib.rs
+++ ch7/restaurant/src/lib.rs
@@ -1,12 +1,41 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
+mod front_of_house;
     
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
+use crate::front_of_house::hosting;
+
+mod customer {
+   
+
+    pub fn eat_at_restaurant() {
+        super::hosting::add_to_waitlist();
     }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }   
+
+    fn cook_order() {}
 }
---
+++ ch7/test_result/src/lib.rs
@@ -0,0 +1,13 @@
+use std::collections::*;
+use std::io::{self, Write};
+use std::{cmp::Ordering, fmt};
+
+fn function1() -> fmt::Result {
+    Ok(())
+}
+
+fn function2() -> io::Result<()> {
+    Ok(())
+}
+
+fn main() {}
