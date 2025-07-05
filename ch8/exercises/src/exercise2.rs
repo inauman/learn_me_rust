2bba1f30b291bb58d2dc7abefca370e86306813c
Mohammad Nauman <md.nauman@gmail.com>
2025-07-05 14:26:53 -0700

    exercise 2

---
+++ ch8/exercises/src/exercise2.rs
@@ -0,0 +1,37 @@
+//Convert strings to pig latin. The first consonant of each word is moved
+// to the end of the word and ay is added, so first becomes irst-fay.
+//  Words that start with a vowel have hay added to the end instead
+// (apple becomes apple-hay). Keep in mind the details about UTF-8 encoding!
+
+use std::io;
+fn is_vowel(c: char) -> bool {
+    matches!(c, 'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U')
+}
+pub fn exercise_2() -> String {
+    //let the user input a string
+    println!("Enter a string: ");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input.trim();
+
+    //split the string into words
+    let words = input.split_whitespace();
+    let mut pig_latin_words = Vec::new();
+    //convert each word to pig latin
+    for word in words {
+       let mut chars = word.chars();
+
+       if let Some(first_char) = chars.next() {
+        let rest: String = chars.collect();
+        let pig_word = if is_vowel(first_char) {
+            format!("{}-hay", word.to_lowercase())
+        } else {
+            format!("{}-{}ay", rest.to_lowercase(), first_char.to_lowercase())
+        };
+        pig_latin_words.push(pig_word);
+       }
+    }
+    pig_latin_words.join(" ")
+}
--- ch8/exercises/src/main.rs
+++ ch8/exercises/src/main.rs
@@ -1,5 +1,7 @@
 mod exercise1;
+mod exercise2;
 use crate::exercise1::{exercise_1_median, exercise_1_mode};
+use crate::exercise2::exercise_2;
 fn main() {
     println!("\n--------------------------------");
     let median = exercise_1_median();
@@ -14,4 +16,7 @@ fn main() {
         Some(m) => println!("Mode: {:?}", m),
         None => println!("No mode"),
     }
+
+    let pig_latin = exercise_2();
+    println!("Pig Latin: {:?}", pig_latin);
 }
